251e2a67a936aace07e86f689642285d38b812b3
Kislay0 <144163496+Kislay0@users.noreply.github.com>
2025-03-19 19:17:25 +0530

    Time: 21 ms (6.05%) | Memory: 140.1 MB (47.55%) - LeetSync

---
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        auto flip = [&] (int left){
+            for (int i = left; i <= left + 2; i++){
+                if (nums[i] == 0){
+                    nums[i] = 1;
+                }
+                else nums[i] = 0;
+            }
+        };
+        int n = nums.size();
+        int cnt = 0;
+        for (int i = 0; i <= n - 3; i++){
+            if (nums[i] == 0){
+                flip(i);
+                cnt++;
+            }
+        }
+        if (nums.back() == 1 & nums[n-2] == 1 ) return cnt;
+        return -1;
+    }
+};
