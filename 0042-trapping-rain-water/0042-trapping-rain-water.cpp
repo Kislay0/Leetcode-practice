9658f209cc7b372373708af37e1eab1d764e5a81
Kislay0 <144163496+Kislay0@users.noreply.github.com>
2025-03-28 21:42:37 +0530

    Time: 0 ms (100%), Space: 26 MB (79.85%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -2,22 +2,31 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n = height.size();
-        vector<pair<int,int>> bounds (n);
-        bounds[0].first = 0;
-        bounds[n-1].second = 0;
-        int i = 0, j = n-1;
-        int lmax = 0, rmax =0;
-        while(i<n){
-            bounds[i].first =  lmax;
-            lmax = max(lmax, height[i++]);
-            bounds[j].second = rmax;
-            rmax = max(rmax, height[j--]);
+        int gmax = 0;
+        int temp = 0;
+        for (int i = 0; i<n; i++){
+            if (height[i]>=temp){
+                temp = height[i];
+                gmax = i;
+            }
         }
+        // cout<<"gmax: "<<gmax<<endl;
+        int lmax = 0;
         int ans = 0;
-        for (int i = 0; i< n; i++){
-            int ldepth = ((bounds[i].first - height[i]) > 0) ? (bounds[i].first - height[i]) : 0;
-            int rdepth = ((bounds[i].second - height[i]) > 0) ? (bounds[i].second - height[i]) : 0;
-            ans += min(ldepth, rdepth);
+        for (int i = 0; i<gmax; i++){
+            if (lmax > height[i]) {
+                ans += lmax - height[i];
+                // cout<<i<<" val: "<<height[i]<<" 1: "<<lmax - height[i]<<endl;
+            }
+            else lmax = height[i];
+        }
+        lmax = 0;
+        for (int i = n - 1; i > gmax; i--){
+            if (lmax > height[i]) {
+                ans += lmax - height[i];
+                // cout<<i<<" val: "<<height[i]<<" 1: "<<lmax - height[i]<<endl;
+            }
+            else lmax = height[i];
         }
         return ans;
     }
