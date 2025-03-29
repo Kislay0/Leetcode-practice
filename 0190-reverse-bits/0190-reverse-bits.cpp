81197a37c5cf641b75316a495a5240d3c382b320
Kislay0 <144163496+Kislay0@users.noreply.github.com>
2025-03-29 23:26:27 +0530

    Time: 5 ms (10.05%), Space: 7.9 MB (27.93%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -3,8 +3,13 @@ public:
     uint32_t reverseBits(uint32_t n) {
         uint32_t rbits = 0;
         for (int i = 0; i<32; i++){
-            rbits = (rbits<<1) | (n&1);
+            rbits = (rbits<<1);
+            rbits = (rbits | (n&1));
             n = n>>1;
+            if (!n) {
+                rbits = rbits<<(31-i);
+                break;
+            }
         }
         return rbits;        
     }
