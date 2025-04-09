b021fdea6e46bdd6cd0bd27d2cc955b9fb0d4199
Kislay Tiwari <144163496+Kislay0@users.noreply.github.com>
2025-04-09 22:11:27 +0530

    Time: 12 ms (55.35%), Space: 32.4 MB (42.14%) - LeetHub

--- 3621-minimum-operations-to-make-array-values-equal-to-k/3621-minimum-operations-to-make-array-values-equal-to-k.cpp
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/3621-minimum-operations-to-make-array-values-equal-to-k.cpp
@@ -1,3 +1,4 @@
+
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
@@ -5,7 +6,7 @@ public:
         int lo = INT_MAX;
         for (int num: nums){
             freq.insert(num);
-            lo = min(lo, num);
+            if (lo > num) lo = num;
         }
         if (lo < k) return -1;
         if (lo == k) return freq.size()-1;
