9e69b856651baa5055f9aab2056415a936ce0584
Kislay Tiwari <144163496+Kislay0@users.noreply.github.com>
2025-04-09 22:10:13 +0530

    Time: 11 ms (64.47%) | Memory: 32.6 MB (11.32%) - LeetSync

--- 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.cpp
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
-        set<int> freq;
+        unordered_set<int> freq;
+        int lo = INT_MAX;
         for (int num: nums){
             freq.insert(num);
+            lo = min(lo, num);
         }
-        int itr = *freq.begin();
-        if (itr < k) return -1;
-        if (itr == k) return freq.size()-1;
+        if (lo < k) return -1;
+        if (lo == k) return freq.size()-1;
         return freq.size();
     }
 };
