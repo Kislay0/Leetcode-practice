bacf5a7ff29179a19d8c8c49fa2df75664de258e
Kislay Tiwari <144163496+Kislay0@users.noreply.github.com>
2025-04-10 16:15:16 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (6.52%) - LeetSync

--- 60-permutation-sequence/permutation-sequence.cpp
+++ 60-permutation-sequence/permutation-sequence.cpp
@@ -1,43 +1,32 @@
 class Solution {
-public:
-    int greatest_lower (unordered_map<int, int>fact, int a){
-        int lo = 1, hi = 9;
-        int mid, ans = 1;
-        while(hi >= lo){
-            mid = (lo+hi)>>1;
-            if (fact[mid] == a) return mid;
-            if (fact[mid] > a){
-                hi = mid-1;
-            }
-            else {
-            ans = mid;
-            lo = mid+1;
-            }
+private:
+static const unordered_map <int,int> fact;
+int greatest_lower (int a){
+    int lo = 1, hi = 9;
+    int mid, ans = 1;
+    while(hi >= lo){
+        mid = (lo+hi)>>1;
+        if (fact.at(mid) == a) return mid;
+        if (fact.at(mid) > a){
+            hi = mid-1;
         }
-        // cout<<ans<<endl;
-        return ans;
+        else {
+        ans = mid;
+        lo = mid+1;
+        }
     }
+    return ans;
+}
+public:
     string getPermutation(int n, int k) {
-        unordered_map <int,int> fact = {
-            {1, 1},
-            {2, 2},
-            {3, 6},
-            {4, 24},
-            {5, 120},
-            {6, 720},
-            {7, 5040},
-            {8, 40320},
-            {9, 362880}
-        };
         vector<int> available;
         for (int i = 1; i<= n; i++){
             available.push_back(i);
         }
-        //
+
         string ans = "";
-        int arranged, left = greatest_lower(fact, k);
-        // cout<<left<<endl;
-        if (k-fact[left] == 0){
+        int arranged, left = greatest_lower(k);
+        if (k-fact.at(left) == 0){
             arranged = n - left;
             for (int i = 1; i <= arranged; i++){
                 available.erase(available.begin());
@@ -51,19 +40,13 @@ public:
                 ans += to_string(i);
             }
         }
-        // cout<<ans<<endl;
-        // cout<< "arranged: " << arranged << endl;
-        // cout<<"left: "<<left<<endl;
-
         while(k>=0){
             int cnt = 0;
             left--;
-            // cout<<"K "<<k<<" left "<<left<<endl;
             if (left == 0) return ans + to_string(available[0]);
-            while (k>fact[left]){
+            while (k>fact.at(left)){
                 cnt++;
-                k -= fact[left];
-                // cout<<"K "<<k<<" cnt "<<cnt<<endl;
+                k -= fact.at(left);
             }
             ans += to_string(available[cnt]);
             available.erase(available.begin() + cnt);
@@ -75,4 +58,16 @@ public:
         return ans;
 
     }
+};
+
+const unordered_map<int, int> Solution::fact = {
+    {1, 1},
+    {2, 2},
+    {3, 6},
+    {4, 24},
+    {5, 120},
+    {6, 720},
+    {7, 5040},
+    {8, 40320},
+    {9, 362880}
 };
