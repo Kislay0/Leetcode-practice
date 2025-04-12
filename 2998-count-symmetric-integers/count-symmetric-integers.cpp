c276e0f26d802426ac39b8c2cf10d4c04b2e23c1
Kislay Tiwari <144163496+Kislay0@users.noreply.github.com>
2025-04-12 23:20:04 +0530

    Time: 4 ms (91.42%) | Memory: 9.5 MB (90.34%) - LeetSync

--- 2998-count-symmetric-integers/count-symmetric-integers.cpp
+++ 2998-count-symmetric-integers/count-symmetric-integers.cpp
@@ -3,10 +3,10 @@ public:
     int countSymmetricIntegers(int low, int high) {
         int ans = 0;
         for (int i = low; i <= high; i++) {
-            if (i<10 || (i>=100 && i<1000)) {
-                i = pow(10, (int)log10(i)+1);
-                continue;
-            }
+            // if (i<10 || (i>=100 && i<1000)) {
+            //     i = pow(10, (int)log10(i)+1);
+            //     continue;
+            // }
             if(10<=i && i<100 && i%11==0){
                 ans++;
             }else if(1000<i && i<10000){
