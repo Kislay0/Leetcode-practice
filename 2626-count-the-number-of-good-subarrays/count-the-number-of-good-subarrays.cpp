4f1dddff9e089360e7677d58cabe8b93e60a6383
Kislay Tiwari <144163496+Kislay0@users.noreply.github.com>
2025-06-18 01:23:30 +0530

    Time: 72 ms (84.84%) | Memory: 78.6 MB (93.44%) - LeetSync

--- 2626-count-the-number-of-good-subarrays/count-the-number-of-good-subarrays.cpp
+++ 2626-count-the-number-of-good-subarrays/count-the-number-of-good-subarrays.cpp
@@ -8,21 +8,12 @@ public:
         long long pairs = 0;
         unordered_map<int,int> mp;
         int left = 0;
-        int n = nums.size();
+        const int n = nums.size();
         for (int i = 0; i< n; i++){
-            mp[nums[i]]++;
-            if (mp[nums[i]] > 2){
-                pairs += mp[nums[i]] - 1;
-            }
-            else if (mp[nums[i]] > 1) {
-                pairs += (mp[nums[i]] * (mp[nums[i]]-1))>>1;
-            }
+            pairs  += mp[nums[i]]++;
             while (pairs >= k){
                 ans += n-i;
-                mp[nums[left]]--;
-                if (mp[nums[left]] < 1) mp.erase(nums[left]);
-                else if (mp[nums[left]] > 1) pairs -=  mp[nums[left]];
-                else pairs -= mp[nums[left]] * (mp[nums[left]] + 1) / 2;
+                pairs -= --mp[nums[left]];
                 left++;
             }
         }
