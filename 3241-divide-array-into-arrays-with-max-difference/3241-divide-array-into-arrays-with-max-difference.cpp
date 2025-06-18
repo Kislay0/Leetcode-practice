34c2aca432b7e906b03bbd1270a997e2ee539552
Kislay Tiwari <144163496+Kislay0@users.noreply.github.com>
2025-06-18 11:59:41 +0530

    Time: 49 ms (56.46%), Space: 120.2 MB (44.5%) - LeetHub

--- 3241-divide-array-into-arrays-with-max-difference/3241-divide-array-into-arrays-with-max-difference.cpp
+++ 3241-divide-array-into-arrays-with-max-difference/3241-divide-array-into-arrays-with-max-difference.cpp
@@ -4,9 +4,8 @@ public:
         sort(nums.begin(),nums.end());
         vector<vector<int>> ans;
         for (int i = 0; i< nums.size(); i+=3){
-            if (nums[i+2]- nums[i] > k) return {};
-            vector<int> temp ={nums[i], nums[i+1], nums[i+2]};
-            ans.push_back(temp);
+            if (nums[i+2] - nums[i] > k) return {};
+            ans.push_back({nums[i], nums[i+1], nums[i+2]});
         }
         return ans;
     }
