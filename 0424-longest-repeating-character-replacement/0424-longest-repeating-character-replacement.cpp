6c0a6e678c2461b11f1e4f4dc21a3407c24c47fc
Kislay Tiwari <144163496+Kislay0@users.noreply.github.com>
2025-06-26 23:53:25 +0530

    Time: 26 ms (10.43%), Space: 11 MB (41.01%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,9 +1,5 @@
 class Solution {
 public:
-    int maxind (vector<int> v){
-        auto maxIt = max_element(v.begin(), v.end());
-        return distance(v.begin(), maxIt);
-    }
     int characterReplacement(string s, int k) {
         vector<int> freq (26);
         int i = 0, j = 0;
@@ -12,7 +8,6 @@ public:
             freq[s[j++]-'A']++;
             len++;
             while ((len - *max_element(freq.begin(), freq.end()))> k && i<=j){
-                cout<<i<<" "<<j<<endl;
                 freq[s[i++]-'A']--;
                 len--;
             }
