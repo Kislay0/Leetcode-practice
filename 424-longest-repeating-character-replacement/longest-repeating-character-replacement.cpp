6a7c549ddc47243fc467fa4853d56e7849c789aa
Kislay Tiwari <144163496+Kislay0@users.noreply.github.com>
2025-06-27 00:28:09 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (41.01%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    int maxind (vector<int> v){
-        auto maxIt = max_element(v.begin(), v.end());
-        return distance(v.begin(), maxIt);
-    }
-    int characterReplacement(string s, int k) {
-        vector<int> freq (26);
-        int i = 0, j = 0;
-        int len = 0, maxlen = 0;
-        while (j<s.size()){
-            freq[s[j++]-'A']++;
-            len++;
-            while ((len - *max_element(freq.begin(), freq.end()))> k && i<=j){
-                cout<<i<<" "<<j<<endl;
-                freq[s[i++]-'A']--;
-                len--;
+    int characterReplacement(string s, int k)
+    {
+        int l=0;
+        int len=0;
+        int maxcount=0;
+        vector<int>count(26,0);
+        for(int r=0;r<s.size();r++)
+        {
+            count[s[r]-'A']++;
+            maxcount=max(maxcount,count[s[r]-'A']);
+            while((r-l+1)-maxcount>k)
+            {
+                count[s[l]-'A']--;
+                l++;
             }
-            maxlen = max(len, maxlen);
+            len=max(len,r-l+1);
         }
-        return maxlen;
+        return len;
+
     }
 };
