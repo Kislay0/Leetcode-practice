612d9ee90b246341692e38d2f1aac05ff6be6238
Kislay Tiwari <144163496+Kislay0@users.noreply.github.com>
2025-08-18 14:06:31 +0530

    Time: 2 ms (72.53%), Space: 65.2 MB (28.22%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -4,11 +4,14 @@ public:
         int slow = 0;
         int fast = 0;
         do{
+            // cout<<slow<<" "<<fast<<endl;
             fast = nums[nums[fast]];
             slow = nums[slow];
         }while( slow!= fast);
+        cout<<fast<<endl;
         slow = 0;
         while(slow!= fast){
+            // cout<<slow<<" "<<fast<<endl;
             slow = nums[slow];
             fast = nums[fast];
         }
