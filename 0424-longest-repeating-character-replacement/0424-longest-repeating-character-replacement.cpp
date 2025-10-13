51dd82b50e38c90c08c5ad63eafa75285d9962e0
Kislay Tiwari <144163496+Kislay0@users.noreply.github.com>
2025-10-14 05:28:44 +0530

    Time: 4 ms (58.16%), Space: 10.9 MB (40.22%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
-    int characterReplacement(string s, int k)
-    {
-        int l=0;
-        int len=0;
-        int maxcount=0;
-        vector<int>count(26,0);
-        for(int r=0;r<s.size();r++)
-        {
-            count[s[r]-'A']++;
-            maxcount=max(maxcount,count[s[r]-'A']);
-            while((r-l+1)-maxcount>k)
-            {
-                count[s[l]-'A']--;
-                l++;
+    int characterReplacement(string s, int k) {
+        int mlen = 0, mcnt = 0;
+        vector<int> freq(26,0);
+        int lo = 0;
+        for (int hi = 0; hi< (int) s.size(); hi++){
+            freq[s[hi]-'A']++;
+            mcnt = max(mcnt, freq[s[hi]- 'A']);
+            while((hi - lo + 1) - mcnt > k){
+                // cout<<lo<<" "<<hi<<" ";
+                // cout<<mcnt<<endl;
+                freq[s[lo++]-'A']--;
             }
-            len=max(len,r-l+1);
+            mlen = max(mlen, hi - lo + 1);
         }
-        return len;
-
+        return mlen;
     }
 };
