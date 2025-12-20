632938e48589c7635edee7e098ebf7bede6c2254
Kislay Tiwari <144163496+Kislay0@users.noreply.github.com>
2025-12-21 01:38:57 +0530

    Time: 785 ms (32.83%), Space: 11.7 MB (22.78%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,41 +1,37 @@
 class Solution {
 private:
-    bool sol(vector<vector<char>>& board, string word, vector<vector<bool>>& flag, int index, int i, int j){
-        // cout<<"\nPlace 1 "<<i<<":"<<j<<" ";
+    bool sol(vector<vector<char>>& board, string word,  int index, int i, int j){
         if (i<0 || j<0 || i>= board.size() || j >= board[0].size()) return false;
-        // cout<<"Place 2 "<<flag[i][j];
-        if (flag[i][j]) return false;
-        // cout<<" "<<word[index]<<":"<<board[i][j]<<" ";
         if (word[index] != board[i][j]) return false;
-        // cout<<"HERE ";
         index++;
-        flag[i][j] = true;
+        char temp = board[i][j];
+        board[i][j] = '0';
         if (index == word.size()) return true;
-        bool up = sol(board, word, flag, index, i-1, j);
-        bool down = sol(board, word, flag, index, i+1, j);
-        bool left = sol(board, word, flag, index, i, j-1);
-        bool right = sol(board, word, flag, index, i, j+1);
+        bool up = sol(board, word, index, i-1, j);
+        bool down = sol(board, word, index, i+1, j);
+        bool left = sol(board, word, index, i, j-1);
+        bool right = sol(board, word, index, i, j+1);
         if (up || down || left|| right) return true;
-        flag[i][j] = false;
+        board[i][j] = temp;
         return false;
     }
 public:
     bool exist(vector<vector<char>>& board, string word) {
+        int len = board.size()*board[0].size();
+        if (len<word.size()) return false;
         vector<bool>temp(board[0].size(), false);
-        vector<vector<bool>> flag(board.size(), temp);
-        // vector<vector<bool>> check(board[0].size(), temp);
         for (int i = 0; i< board.size(); i++){
             for (int j = 0; j<board[0].size(); j++){
                 if (board[i][j] == word[0]){
                     if (word.size() == 1) return true;
-                    // cout<<"\ndebug"<<endl;
-                    flag[i][j] = true;
-                    bool up = sol(board, word, flag, 1, i-1, j);
-                    bool down = sol(board, word, flag, 1, i+1, j);
-                    bool left = sol(board, word, flag, 1, i, j-1);
-                    bool right = sol(board, word, flag, 1, i, j+1);
+                    char temp = board[i][j];
+                    board[i][j] = '0';
+                    bool up = sol(board, word, 1, i-1, j);
+                    bool down = sol(board, word, 1, i+1, j);
+                    bool left = sol(board, word, 1, i, j-1);
+                    bool right = sol(board, word, 1, i, j+1);
                     if(up || down || left|| right) return true;
-                    flag[i][j] = false;
+                    board[i][j] = temp;
                 }
             }
         }
